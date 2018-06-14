d4b622816c0e571d16084c5dd45edf943e1a85c4
Bishal Baaniya <071bct513@pcampus.edu.np>
2018-06-14 19:08:53 +0545

    final push from office

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 mongoose.connect('mongodb://localhost/nodekb');
 let db = mongoose.connection;
@@ -31,6 +34,35 @@ app.use(bodyParser.json());
 //Set Public Folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Express session Middleware
+app.use(session({
+    secret: 'keyboard cat',
+    resave: true,
+    saveUninitialized: true,
+}));
+//Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+    res.locals.messages = require('express-messages')(req, res);
+    next();
+});
+//Express Validator Middleware
+app.use(expressValidator({
+    errorFormatter: function (param, msg, value) {
+        var namespace = param.split('.')
+            , root = namespace.shift()
+            , formParam = root;
+
+        while (namespace.length) {
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param: formParam,
+            msg: msg,
+            value: value
+        };
+    }
+}));
 //Home Route
 app.get('/', function (req, res) {
     Article.find({}, function (err, articles) {
@@ -57,6 +89,20 @@ app.get('/article/:id', function (req, res) {
     });
 });
 
+//Load Edit Form
+app.get('/article/edit/:id', function (req, res) {
+    Article.findById(req.params.id, function (err, article) {
+        if (err) {
+            console.log(err);
+        } else {
+            res.render("edit_article", {
+                title: "Edit Article",
+                article: article
+            });
+        }
+    });
+});
+
 //Add Route
 app.get('/articles/add', function (req, res) {
     res.render("add_article", {
@@ -65,18 +111,62 @@ app.get('/articles/add', function (req, res) {
 });
 //Add Submit POST route
 app.post('/articles/add', function (req, res) {
-    let article = new Article();
+    req.checkBody('title', 'Title is required').notEmpty;
+    req.checkBody('author', 'Author is required').notEmpty;
+    req.checkBody('body', 'Body is required').notEmpty;
+
+    //Get Errors
+    let errors = req.validationErrors();
+    if (errors) {
+        res.render('add_article', {
+            title: 'Add Article',
+            errors: errors
+        })
+    } else {
+        let article = new Article();
+        article.title = req.body.title;
+        article.author = req.body.author;
+        article.body = req.body.body;
+
+        article.save(function (err) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                req.flash('success', 'Article Added');
+                res.redirect('/');
+            }
+        });
+    }
+});
+//Edit Submit POST route
+app.post('/articles/edit/:id', function (req, res) {
+    let article = {};
     article.title = req.body.title;
     article.author = req.body.author;
     article.body = req.body.body;
 
-    article.save(function (err) {
+    let query = {_id: req.params.id};
+
+    Article.update(query, article, function (err) {
         if (err) {
             console.log(err);
             return;
         } else {
+            req.flash('success', 'Article Updated');
             res.redirect('/');
         }
+    });
+});
+
+app.delete('/article/:id', function (req, res) {
+    let query = {_id: req.params.id};
+
+    Article.remove(query, function (err) {
+        if (err) {
+            console.log(err)
+        }
+        res.send('Success');
     });
 });
 //Start Server
